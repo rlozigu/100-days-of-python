0ebe70648a9c29bda41b322caa9c5197573526db
rlozigu <rlozigu@gmail.com>
2023-05-31 14:26:59 +0900

    rules of naming variables

--- practice/variables.py
+++ practice/variables.py
@@ -6,3 +6,9 @@ print(name)
 
 name = input("What is your name? ")
 print(len(name))
+
+#rules of naming variables
+#name has to be one single unit
+#in order to seperate word, use "_"
+#number can't be beginning of variable
+user_name = "name"
